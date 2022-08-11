f5db5a6ecb5f9072d037de4a1a3422e05c155ba6
Dylan Cramer <dcrame2@gmail.com>
2022-08-11 09:17:51 -0500

    Finished product for now

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from './Card';
 import Button from './Button';
 import styles from './ErrorModal.module.css';
+import ReactDOM from 'react-dom';
 
 const Backdrop = (props) => {
     return  <div className={styles.backdrop} onClick={props.onConfirm}/>
@@ -26,9 +27,9 @@ const ModalOverlay = props => {
 const ErrorModal = props => {
     return (
         <React.Fragment>
-           
-            
-         </React.Fragment>
+           {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />, document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm} />, document.getElementById('overlay-root'))}
+        </React.Fragment>
     )
 };
 
